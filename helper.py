9392a1c33730e27bc2d404ceae48e28ea0588c31
sqy-harshil <harshil.patel@squareyards.co.in>
2023-08-22 14:16:17 +0530

    Fixed Openai error

--- helper.py
+++ helper.py
@@ -11,7 +11,7 @@ from fastapi import HTTPException
 from named_tuples import AnalysisJSON
 
 openai.api_base = "https://api.openai.com/v1"
-openai.api_key = os.getenv("SQY_API_KEY")
+openai.api_key = os.getenv("OPENAI_API_KEY")
 openai.api_type = "open_ai"
 openai.api_version = "2020-10-01"
 
